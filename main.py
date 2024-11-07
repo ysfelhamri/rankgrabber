0f2eb355deb25ab11a0f9b877c8fe42ae65c72a9
Youssef EL HAMRI <103438312+ysfelhamri@users.noreply.github.com>
2024-11-07 13:48:38 +0100

    Updated main.py
    Updated the program to work with the new changes implemented by Riot Games.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-from riotwatcher import LolWatcher, ApiError
+from riotwatcher import RiotWatcher, LolWatcher, ApiError
 import tkinter
 import customtkinter
 
@@ -10,32 +10,34 @@ window.geometry("300x380")
 window.resizable(False, False)
 window.title("Rank grabber")
 
-
-regions = ["EUW1","NA1","EUN1","BR1","JP1","KR","LA1","OC1","RU","TR1"]
+account_regions = ["EUROPE","AMERICAS","ASIA"]
+regions = ["EUW1","EUN1","NA1","BR1","LA1","LA2","JP1","KR","OC1"]
 queuetypes = ["Solo/Duo","Flex"]
-errdis = ["Success","api_key.txt doesn't exist \n or can't be opened","Invalid api key","Enter a name"
-,"Invalid name","output.txt can't be created","No internet","Unknown error"]
+errdis = ["Success","api_key.txt doesn't exist \n or can't be opened","Invalid api key","Please fill all fields"
+,"Invalid name","output.txt can't be created","A connection error has occurred","An unknown error has occurred"]
 
 def showerr(errindx):
-    dis.configure(text = errdis[errindx])
+    if errindx == 0:
+        dis.configure(text = errdis[errindx],text_color = "Green")
+    else :
+        dis.configure(text = errdis[errindx],text_color = "Red")
     dis.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
 
 def callapi():
     
     try:
         requests.head("http://google.com",timeout=1)
-    except requests.ConnectionError:
+    except Exception:
         showerr(6)
         return
 
 
     if dis.winfo_viewable():
-        if dis.__getattribute__("text_color") != "Red":
-            dis.configure(text_color = "Red")
         dis.place_forget()
 
 
     name = entry1.get()
+    tag = tagtxt.get()
     region = rankmenu.get()
     if (qmenu.get()==queuetypes[0]):
         q="RANKED_SOLO_5x5"
@@ -50,15 +52,42 @@ def callapi():
         return
 
     api_key = open("api_key.txt",'r')
+    api_txt = api_key.read().strip()
 
     
-    if(len(name)==0 or len(region)==0):
+    if(len(name)==0 or len(region)==0 or len(tag)==0):
         showerr(3)
         return
 
-    lolw=LolWatcher(api_key.read().strip())
+    accregion = ""
+    if region == regions[0] or region == regions[1]:
+        accregion = account_regions[0]
+    elif region == regions[2] or region == regions[3] or region == regions[4] or region == regions[5]:
+        accregion = account_regions[1]
+    elif region == regions[6] or region == regions[7] or region == regions[8]:
+        accregion = account_regions[2]
+
+    riotw=RiotWatcher(api_txt)
+    account = ""
     try:
-        lolw.summoner.by_name(region, name)
+        account = riotw.account.by_riot_id(accregion,name,tag)
+    except ApiError as err:
+        print(err)
+        if err.response.status_code == 404:
+            showerr(4)
+            return
+        elif err.response.status_code == 403:
+            showerr(2)
+            return
+        else:
+            showerr(7)
+            return
+
+
+
+    lolw=LolWatcher(api_txt)
+    try:
+        lolw.summoner.by_puuid(region, account['puuid'])
     except ApiError as err:
         if err.response.status_code == 404:
             showerr(4)
@@ -69,7 +98,7 @@ def callapi():
         else:
             showerr(7)
             return
-    userinfo = lolw.summoner.by_name(region,name)
+    userinfo = lolw.summoner.by_puuid(region,account['puuid'])
     rank = lolw.league.by_summoner(region,userinfo['id'])
 
     try:
@@ -81,7 +110,7 @@ def callapi():
     output = open("output.txt","w", encoding="utf-8")
 
     if(len(rank)==0):
-        txt = [userinfo['name']," is unranked"]
+        txt = [name," is unranked"]
         output.writelines(txt)
     else:
         flag =0
@@ -90,10 +119,10 @@ def callapi():
                 disrnk = rnk
                 flag =1
         if flag==0:
-            txt = [userinfo['name']," is unranked"]
+            txt = [name," is unranked"]
             output.writelines(txt)
         else:
-            txt = [disrnk['summonerName'],"\n",disrnk['tier']," ",disrnk['rank']," ",str(disrnk['leaguePoints'])," LP","\n",str(disrnk["wins"]),'W/',str(disrnk["losses"]),'L']
+            txt = [name,"\n",disrnk['tier']," ",disrnk['rank']," ",str(disrnk['leaguePoints'])," LP","\n",str(disrnk["wins"]),'W/',str(disrnk["losses"]),'L']
             output.writelines(txt)
 
     dis.configure(text_color= "Green") 
@@ -118,8 +147,10 @@ label1= customtkinter.CTkLabel(master = insideframe  , text = "Name :",text_font
 label1.place(relx=0.2, rely=0.2, anchor=tkinter.CENTER)
 
 
-entry1 = customtkinter.CTkEntry(master = insideframe  , width=150, height=40,corner_radius=10,justify='center', text_font=('',12),fg_color='#2B2D2F')
-entry1.place(relx=0.65, rely=0.2, anchor=tkinter.CENTER)
+entry1 = customtkinter.CTkEntry(master = insideframe  , width=95, height=40,corner_radius=10,justify='center', text_font=('',12),fg_color='#2B2D2F')
+tagtxt = customtkinter.CTkEntry(master = insideframe  , width=50, height=40,corner_radius=10,justify='center', text_font=('',12),fg_color='#2B2D2F', placeholder_text="#")
+entry1.place(relx=0.54, rely=0.2, anchor=tkinter.CENTER)
+tagtxt.place(relx=0.85, rely=0.2, anchor=tkinter.CENTER)
 
 
 
